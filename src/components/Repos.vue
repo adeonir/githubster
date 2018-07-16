07dd0d80dab57c9492d1593290912e53402d356e
Adeonir Kohl <adeonir@gmail.com>
2018-07-16 19:31:05 -0300

    Initial commit

---
+++ src/components/Repos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="repos">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Repos',
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="search">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props: {}
+};
+</script>
+
+<style scoped lang="sass">
+
+</style>
